96525602a758764f1ab4ab662b3ef7a3a02df062
tr8yutufjh <105630242+tr8yutufjh@users.noreply.github.com>
2023-12-22 13:45:38 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,22 +7,22 @@ fi
 echo ${VER}
 
 if [[ -z "${Vless_Path}" ]]; then
-  Vless_Path="/394fe03d-289a-4dea-b9b5-0bed468260f3"
+  Vless_Path="/66eb41d1-bfe9-4121-b40d-d48caa11b5c8"
 fi
 echo ${Vless_Path}
 
 if [[ -z "${Vless_UUID}" ]]; then
-  Vless_UUID="394fe03d-289a-4dea-b9b5-0bed468260f3"
+  Vless_UUID="66eb41d1-bfe9-4121-b40d-d48caa11b5c8"
 fi
 echo ${Vless_UUID}
 
 if [[ -z "${Vmess_Path}" ]]; then
-  Vmess_Path="/5d293e61-305e-4003-ac87-4587b36646aa"
+  Vmess_Path="/3c14a51d-cbb0-4e19-ac83-387d1e412e8f"
 fi
 echo ${Vmess_Path}
 
 if [[ -z "${Vmess_UUID}" ]]; then
-  Vmess_UUID="5d293e61-305e-4003-ac87-4587b36646aa"
+  Vmess_UUID="3c14a51d-cbb0-4e19-ac83-387d1e412e8f"
 fi
 echo ${Vmess_UUID}
 
